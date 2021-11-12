00fa3fb54155eecf68cda05a4857cbfbec901af4
Farrukh Shaikh <52906883+FarrukhShaikh21@users.noreply.github.com>
2021-11-12 22:48:24 +0500

    adf globals

--- ERPSolGlobViewController/src/erpsolglob/erpsolglobview/erpclass/ERPSolGlobalViewBean.java
+++ ERPSolGlobViewController/src/erpsolglob/erpsolglobview/erpclass/ERPSolGlobalViewBean.java
@@ -1,7 +1,23 @@
 package erpsolglob.erpsolglobview.erpclass;
 
+import oracle.adf.share.ADFContext;
+
 public class ERPSolGlobalViewBean {
     public ERPSolGlobalViewBean() {
         super();
     }
+    public static String doGetUserCode(){
+        return ADFContext.getCurrent().getSessionScope().get("GLOB_USER_CODE").toString();
+    }
+    
+    public static String doGetUserLocation(){
+        return ADFContext.getCurrent().getSessionScope().get("GLOB_USER_LOCATION").toString();
+    }
+    
+    
+    public static String doGetUserStore(){
+        return ADFContext.getCurrent().getSessionScope().get("GLOB_USER_STORE").toString();
+    }
+    
+   
 }
