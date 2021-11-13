ecfc7247e45082c55e4c6e67776e8cc7b0f30ad4
Farrukh Shaikh <52906883+FarrukhShaikh21@users.noreply.github.com>
2021-11-13 21:43:18 +0500

    adding global class

--- ERPSolGlobViewController/src/erpsolglob/erpsolglobview/erpclass/ERPSolGlobalViewBean.java
+++ ERPSolGlobViewController/src/erpsolglob/erpsolglobview/erpclass/ERPSolGlobalViewBean.java
@@ -1,23 +1,42 @@
 package erpsolglob.erpsolglobview.erpclass;
 
+import erpsolglob.erpsolglobmodel.erpsolglobclasses.ERPSolGlobClassModel;
+
 import oracle.adf.share.ADFContext;
+import oracle.binding.BindingContainer;
+import oracle.adf.model.BindingContext;
+import oracle.adf.model.binding.DCBindingContainer;
+import oracle.adf.model.binding.DCDataControl;
+import oracle.adf.model.binding.DCIteratorBinding;
 
 public class ERPSolGlobalViewBean {
     public ERPSolGlobalViewBean() {
         super();
     }
     public static String doGetUserCode(){
-        return ADFContext.getCurrent().getSessionScope().get("GLOB_USER_CODE").toString();
+        return ERPSolGlobClassModel.doGetUserCode();
+//        return ADFContext.getCurrent().getSessionScope().get("GLOB_USER_CODE").toString();
     }
     
-    public static String doGetUserLocation(){
-        return ADFContext.getCurrent().getSessionScope().get("GLOB_USER_LOCATION").toString();
+    public static String doGetUserLocationCode(){
+        return ERPSolGlobClassModel.doGetUserCode();
+//        return ADFContext.getCurrent().getSessionScope().get("GLOB_USER_LOCATION").toString();
     }
     
     
-    public static String doGetUserStore(){
-        return ADFContext.getCurrent().getSessionScope().get("GLOB_USER_STORE").toString();
+    public static String doGetUserStoreCode(){
+        return ERPSolGlobClassModel.doGetUserCode();
     }
-    
-   
+  
+    public static BindingContainer doGetERPBindings() {
+           return BindingContext.getCurrent().getCurrentBindingsEntry();
+       }   
+    public static boolean doIsERPSolTransactionDirty() {
+        //this will check do we have any unsaved changes on form
+        BindingContext context = BindingContext.getCurrent();
+        DCBindingContainer binding = (DCBindingContainer) context.getCurrentBindingsEntry();
+        DCDataControl dc = binding.getDataControl();
+
+        return dc.isTransactionDirty();
+    }    
 }
